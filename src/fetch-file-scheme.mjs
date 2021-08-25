fabd73bc605d1d3072174102324d67fa7f7209c8
Nicolas Vanhoren <nicolas.vanhoren@gmail.com>
2021-08-25 20:27:58 +0200

    First working version

--- src/fetch-file-scheme.mjs
+++ src/fetch-file-scheme.mjs
@@ -1,4 +1,8 @@
 
+import { createReadStream, statSync } from 'fs'
+import { Request, Response } from 'node-fetch'
+import assert from 'assert'
+
 /**
  * Do stuff
  *
@@ -6,8 +10,34 @@
  * @param {any} init init
  * @returns {any} something
  */
-function fetchFile (resource, init) {
-  return 'TODO'
+async function fetchFile (resource, init) {
+  const request = new Request(resource, init)
+  const url = request.url
+  const match = url.match(/^file:\/\/(.*)$/)
+  assert(match, 'Invalid file URL. It must start with "file://"')
+  const filePath = match[1]
+  return new Promise((resolve, reject) => {
+    try {
+      const readStream = createReadStream(filePath)
+      readStream.on('open', function () {
+        try {
+          const size = statSync(filePath).size
+          const response = new Response(readStream, {
+            url: request.url,
+            status: 200,
+            statusText: 'OK',
+            size: size,
+            timeout: request.timeout
+          })
+          resolve(response)
+        } catch (e) {
+          reject(e)
+        }
+      })
+    } catch (e) {
+      reject(e)
+    }
+  })
 }
 
 export { fetchFile }
--- src/fetch-file-scheme.test.mjs
+++ src/fetch-file-scheme.test.mjs
@@ -1,7 +1,15 @@
 
 import { expect, test } from '@jest/globals'
 import { fetchFile } from './fetch-file-scheme.mjs'
+import { join } from 'path'
+import { readFileSync } from 'fs'
 
-test('base', () => {
-  expect(fetchFile()).toBe('TODO')
+const dataPath = join(__dirname, '__test_data')
+
+test('base', async () => {
+  const path = join(dataPath, 'test-base.txt')
+  const referenceText = readFileSync(path, 'utf8')
+  const res = await fetchFile(`file://${path}`)
+  const text = await res.text()
+  expect(text).toStrictEqual(referenceText)
 })
